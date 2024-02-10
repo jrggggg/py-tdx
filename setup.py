79697a21028b57aa0739186900d0d3f0c0db310c
Julien Rossow-Greenberg <jrossowg@villanova.edu>
2024-02-09 21:50:50 -0500

    setup

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-teamdynamix",
-    version="0.1.3",
+    version="0.1.4",
     author="Julien Rossow-Greenberg",
     author_email="julien.rossowgreenberg@villanova.edu",
     description="Python client for interacting with the TeamDynamix ITSM APIs",
+    long_description="Python client for interacting with the TeamDynamix ITSM APIs",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -13,4 +14,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
+    requires=["requests"],
 )
