400dff0efd712d71d641dcd761a05f9fb095fcf0
Julien Rossow-Greenberg <jrossowg@villanova.edu>
2024-02-10 15:45:48 -0500

    real tests

---
+++ tests/test_tdx.py
@@ -0,0 +1,31 @@
+import pytest
+from pytdx.tdx import Tdx
+import os
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+
+conn_info = json.loads(os.getenv("TDX_SB_CONN"))
+
+
+@pytest.fixture
+def tdx_client():
+    return Tdx(
+        username=conn_info[0],
+        password=conn_info[1],
+        hostname=conn_info[2],
+        environment=conn_info[3],
+        asset_app_id=conn_info[4],
+        client_portal_app_id=conn_info[5],
+        ticketing_app_id=conn_info[6],
+        is_admin=conn_info[7],
+    )
+
+
+def test_get_asset(tdx_client):
+    result = tdx_client.get_asset(asset_id=1145451)
+
+    response_test_data = {"ID": 1145451}
+
+    assert result["ID"] == response_test_data["ID"]
