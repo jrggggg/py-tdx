9d49c8b774cf9f0249c62b6dda3a19661a035320
Julien Rossow-Greenberg <jrossowg@villanova.edu>
2024-02-16 14:15:43 -0500

    recursive to_dict

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-teamdynamix",
-    version="0.2.5",
+    version="0.2.6",
     author="Julien Rossow-Greenberg",
     author_email="julien.rossowgreenberg@villanova.edu",
     description="Python client for interacting with the TeamDynamix ITSM APIs",
